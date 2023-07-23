2c3a7a632899fb8bac3f53ac9baee82963092aee
Alberto Russo <alberto@albertorusso.it>
2023-07-23 11:49:29 +0200

    feat: use whick-key to define cmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local wk = require("which-key")
 
 local neoTreeToogle = function()
   require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
@@ -28,22 +29,23 @@ map("n", "<A-6>", "<Cmd>BufferLineGoToBuffer 6<CR>", opts)
 map("n", "<A-7>", "<Cmd>BufferLineGoToBuffer 7<CR>", opts)
 map("n", "<A-8>", "<Cmd>BufferLineGoToBuffer 8<CR>", opts)
 map("n", "<A-9>", "<Cmd>BufferLineGoToBuffer 9<CR>", opts)
--- Pin/unpin buffer
-map("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", opts)
-map("n", "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", opts)
--- Close buffer
-map("n", "<leader>bx", "<Cmd>Bdelete<CR>", opts)
--- Magic buffer-picking mode
-map("n", "<leader>bk", "<Cmd>BufferLinePick<CR>", opts)
-map("n", "<leader>bK", "<Cmd>BufferLinePickClose<CR>", opts)
--- Sort automatically by...
-map("n", "<leader>bt", "<Cmd>BufferLineSortByTabs<CR>", opts)
-map("n", "<leader>bd", "<Cmd>BufferLineSortByDirectory<CR>", opts)
-map("n", "<leader>br", "<Cmd>BufferLineSortByRelativeDirectory<CR>", opts)
 
--- Other:
--- :BarbarEnable - enables barbar (enabled by default)
--- :BarbarDisable - very bad command, should never be used
+-- BufferLine
+wk.register({
+  b = {
+    name = "BufferLine",
+    k = { "<Cmd>BufferLinePick<CR>", "Pick buffer" },
+    K = { "<Cmd>BufferLinePickClose<CR>", "Close buffer" },
+    p = { "<Cmd>BufferLineTogglePin<CR>", "Pin buffer" },
+    P = { "<Cmd>BufferLineGroupClose ungrouped<CR>", "Unpin buffer" },
+    b = { "<Cmd>Bdelete<CR>", "Close buffer" },
+    t = { "<Cmd>BufferLineSortByTabs<CR>", "Sort by tabs" },
+    s = { "<Cmd>BufferLineSortByDirectory<CR>", "Sort by directory" },
+    r = { "<Cmd>BufferLineSortByRelativeDirectory<CR>", "Sort by relative directory" },
+  },
+}, {
+  prefix = "<leader>",
+})
 
 -- neoTree
 vim.keymap.set("n", "<A-t>", neoTreeToogle, { silent = true })
@@ -58,10 +60,23 @@ vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: add file" })
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 -- Zen mode
-vim.keymap.set("n", "<leader>zz", function()
-  require("zen-mode").toggle()
-  vim.wo.wrap = false
-end, { desc = "Zen mode" })
+wk.register({
+  z = {
+    name = "Zen mode",
+    z = { "<Cmd>ZenMode<CR>", "Toggle zen mode" },
+  },
+}, {
+  prefix = "<leader>",
+})
 
 -- Undo tree
-vim.keymap.set("n", "<leader>h", vim.cmd.UndotreeToggle, { desc = "Undo tree" })
+wk.register({
+  h = {
+    name = "Undo tree",
+    s = { "<Cmd>UndotreeShow<CR>", "Show undo tree" },
+    h = { "<Cmd>UndotreeHide<CR>", "Hide undo tree" },
+    t = { "<Cmd>UndotreeToggle<CR>", "Toggle undo tree" },
+  },
+}, {
+  prefix = "<leader>",
+})
